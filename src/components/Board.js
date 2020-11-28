884a1bc76cb982bcb766dc11e788db93ca961f50
Dave VanLaningham <dvanlani@Daves-MacBook-Pro.local>
2020-11-28 18:29:13 -0500

    Added Solve feature - changed styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import Tile from './Tile'
-import dummyBoard from '../dummyBoard'
+import getBoard from '../getBoard'
 import CheckSolutionButton from './CheckSolutionButton'
 import checkIDs from '../checkIDs'
+import getRow from '../utils/getRow'
+import getColumn from '../utils/getColumn'
+import get3by3 from '../utils/get3by3'
+import sleep from '../utils/sleep'
 
 class Board extends React.Component {
     constructor() {
         super()
-        const initTiles = dummyBoard.map(k => {  // creates an array of objects of length 81
+        const boardValues = getBoard()
+        const initTiles = boardValues.map(k => {  // creates an array of objects of length 81
             return {                             // to represent the tiles on the board
                 value: k,
                 isInitial: !!k 
             }
         })
         this.state = {
+            initTiles: initTiles,
             tiles: initTiles,
             selectedTile: null,
             isSolved: false,
@@ -76,7 +82,7 @@ class Board extends React.Component {
             return true                            // return true if you never had to return false
     }
 
-    handleSolve() {
+    handleCheckSolution() {
         let checks = []
         for (let i=0; i < checkIDs.length; i++) {   // This creates an array filled with the outcomes to
             let set = this.getValues(checkIDs[i])   // each of the checks [true,true,false,true... ] etc.
@@ -91,18 +97,158 @@ class Board extends React.Component {
     }
 
     handleBack() {
-        const currentStep = this.state.step                           // get current step
-        const newTiles = this.state.history[currentStep-1]            // get the tiles for the previous step in history
-        const newHistory = this.state.history.slice(0, currentStep)   // make the history forget that last step
-        console.log(newHistory)
-        this.setState({
+        if (this.state.step != 0) {
+            const currentStep = this.state.step                           // get current step
+            const newTiles = this.state.history[currentStep-1]            // get the tiles for the previous step in history
+            const newHistory = this.state.history.slice(0, currentStep)   // make the history forget that last step
+            this.setState({
             ...this.state,
             tiles: newTiles,
             history: newHistory,
             step: currentStep - 1
         })
-    }
+
+        }
         
+    }
+
+    isTerminus(tiles) {
+        for (let i=0; i < 81; i++) {
+            if (tiles[i].value === null) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    isSolution() {
+        let checks = []
+        for (let i=0; i < checkIDs.length; i++) {   // This creates an array filled with the outcomes to
+            let set = this.getValues(checkIDs[i])   // each of the checks [true,true,false,true... ] etc.
+            checks.push(this.checkValidity(set))
+        }
+
+        if (checks.includes(false)) {
+            return false
+        } else {
+            return true
+         }
+    }
+
+    async handleSolve() {
+        let currentTiles = this.state.initTiles
+        let frontier = []
+        
+        while (true) {
+            /* First, check that the tile configuration is a solution, if so
+            change the state of the board to solved */
+            if (this.isTerminus(currentTiles) && this.isSolution(currentTiles)) {
+                break
+            }
+            
+             /* Next get all the possible actions for the current tile configuration */
+            let actions = this.getActions(currentTiles)
+            /* For each action get add the result to the frontier */
+            for (let i=0; i < actions.length; i++) {
+                let action = actions[i]
+                let result = this.getResult(currentTiles, action)
+                frontier.push(result)
+                }   
+            
+                await new Promise(r => setTimeout(r, 50));
+
+            /* set the new tile configuration to the last item in the frontier */
+            currentTiles = frontier.pop()
+            this.setState({
+                ...this.state,
+                tiles: currentTiles
+            })
+        }
+
+        this.setState({
+            ...this.state,
+            isSolved: true,
+            tiles: currentTiles
+        })
+    }
+
+    getPossibleValues(id, tiles) {
+        /* takes in a tile id and a tile configuration and returns an array of
+            legal values on that tile */
+        let tilesToCheck = []
+        let excludedValues = []
+        let possibleValues = []
+
+        tilesToCheck = tilesToCheck.concat(getRow(id), getColumn(id), get3by3(id))
+
+        for (let i=0; i < tilesToCheck.length; i++) {
+            let tile = tiles[tilesToCheck[i]]
+            if (!!tile.value) {
+                excludedValues.push(tile.value)
+            }
+        }
+
+        for (let i=1; i < 10; i++) {
+            if (!excludedValues.includes(i)) {
+                possibleValues.push(i)
+            }
+        }
+        return possibleValues
+    }
+
+    getActions(tiles) {
+        /* this takes in an array of tile objects and returns an array of "action objects"
+        which look like this:
+        action = {
+            tileID: <id of the tile>,
+            value: <possible value>
+        } */
+
+        // find the first open square
+        let firstOpenTile = null
+        let firstOpenTileFound = false
+        let actions = []
+
+        while (!firstOpenTileFound) {
+            for (let i=0; i<81; i++) {
+                if (tiles[80-i].value == null) {
+                    firstOpenTile = 80-i
+                    firstOpenTileFound = true
+                }
+            }
+        }
+
+        const possibleValues = this.getPossibleValues(firstOpenTile, tiles)
+
+        for (let i = 0; i<possibleValues.length; i++) {
+            let actionObject = {
+                tileID: firstOpenTile,
+                value: possibleValues[i]
+            }
+            actions.push(actionObject)
+        }
+        
+        return actions
+    }
+
+    getResult(tiles, action) {
+        /* takes in tiles as the tile configuration, and an action object and returns the
+        resulting tile configuration */
+        
+        const tileID = action.tileID
+        const value = action.value
+
+        let newTiles = tiles.slice()
+        newTiles[tileID] = {
+            value: value,
+            isInitial: false
+        }
+        
+        return newTiles
+    }
+
+
+    /* Render Method */
     render() {
         const board = this.state.tiles.map((tile, i) => 
             <Tile 
@@ -119,16 +265,20 @@ class Board extends React.Component {
             onKeyDown={e => this.handleKeyDown(e)}>
                 {board}
                 <CheckSolutionButton
-                    onClick={() => this.handleSolve()} />
-
-                <span id="solvedSpan">
-                    {this.state.isSolved ? "You Got it!":""}
-                </span>
+                    onClick={() => this.handleCheckSolution()} />
                 <button 
                 className="toggleHistoryButton"
                 onClick={() => this.handleBack()}>
-                    Back
-                    </button>
+                    Undo
+                </button>
+                <button
+                id="solveButton"
+                onClick={() => this.handleSolve()}>
+                Solve
+                </button>
+                <span id="solvedSpan">
+                    {this.state.isSolved ? "Correct Solution":""}
+                </span>
             </div>
         )
     }
---
+++ src/getBoard.js
@@ -0,0 +1,18 @@
+
+function getBoard() {
+    const boardList = [
+            [5,3,null,null,7,null,null,null,null,
+            6,null,null,1,9,5,null,null,null,
+            null,9,8,null,null,null,null,6,null,
+            8,null,null,null,6,null,null,null,3,
+            4,null,null,8,null,3,null,null,1,
+            7,null,null,null,2,null,null,null,6,
+            null,6,null,null,null,null,2,8,null,
+            null,null,null,4,1,9,null,null,5,
+            null,null,null,null,8,null,null,7,9]
+    ]
+    const randomIndex = Math.floor(Math.random() * boardList.length)
+    return boardList[randomIndex]
+}
+
+export default getBoard
